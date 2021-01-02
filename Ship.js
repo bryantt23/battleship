a37dad509ffe41e4c50c4af846be39819c5cc7e4
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-01 20:09:38 -0800

    Ship factory

--- Ship.js
+++ Ship.js
@@ -1,11 +1,20 @@
 class Ship {
   constructor(length) {
     this.length = length;
-    this.hit = false;
+    this.hitCount = 0;
     this.sunk = false;
   }
 
-  hit(number) {}
+  hit() {
+    this.hitCount++;
+    if (this.hitCount === this.length) {
+      this.sunk = true;
+    }
+  }
+
+  isSunk() {
+    return this.sunk;
+  }
 }
 
 export default Ship;
